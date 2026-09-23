2f0a489b48cdb13901185be051004f7a9c23bf14
José Pereda <jose.pereda@gluonhq.com>
2026-09-23 16:45:51 +0200

    Use JDK26 and remove duplicated symbols (#78)

--- openjdk-ext/src/hotspot/symbol_keeper.cpp
+++ openjdk-ext/src/hotspot/symbol_keeper.cpp
@@ -1,7 +1,3 @@
-extern void* JIMAGE_Open;
-extern void* JIMAGE_Close;
-extern void* JIMAGE_FindResource;
-extern void* JIMAGE_GetResource;
 extern void* JDK_Canonicalize;
 extern void* Java_java_lang_reflect_Array_get;
 extern void* Java_java_lang_reflect_Array_getBoolean;
@@ -452,10 +448,6 @@ extern void* Java_java_util_zip_Inflater_initIDs;
 extern void* Java_java_util_zip_Inflater_reset;
 extern void* Java_java_util_zip_Inflater_setDictionary;
 extern void* Java_java_util_zip_Inflater_setDictionaryBuffer;
-void* duml0 = JIMAGE_Open;
-void* duml1 = JIMAGE_Close;
-void* duml2 = JIMAGE_FindResource;
-void* duml3 = JIMAGE_GetResource;
 void* duml4 = JDK_Canonicalize;
 void* l10 = Java_java_lang_reflect_Array_get;
 void* l11 = Java_java_lang_reflect_Array_getBoolean;
